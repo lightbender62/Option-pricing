ecbaeb2b59718b89082efeb3f3cc6bff76df8c79
lightbender62 <lightbender62@gmail.com>
2026-06-24 05:28:14 +0530

    Added Black-Scholes

---
+++ Black-Scholes.py
@@ -0,0 +1,27 @@
+import math
+from scipy.stats import norm
+
+# Defining variables here 
+S = 45 #Stock price
+K = 40 #Strick price
+T = 0.5 #Expiration Date
+r = 0.1 # Risk-Free Rate
+vol = 0.2 #Volatility (sigma)
+
+#Calculating d1 & d2
+
+d1 = (math.log(S/K) + (r + T * vol**2)*T)/(vol * math.sqrt(T))
+
+d2 = d1 - vol*math.sqrt(T)
+
+#Calculate Call price & Put Price
+
+C = S*norm.cdf(d1) - K* math.exp(-r * T ) * norm.cdf(d2)
+
+P = K*math.exp(-r * T)*norm.cdf(-d2) - S*norm.cdf(-d1)
+
+#Printing Results 
+print('The Value of d1 is : ' , round(d1 , 4))
+print('The Value of d2 is : ' , round(d2 , 4))
+print('The Price of the call option is : $' , round(C , 2))
+print('The Price of the Put option is : $' , round(P , 2))
