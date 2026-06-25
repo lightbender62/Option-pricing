cd3d00afc74254a9a443acc1eb3e2cc1efba6886
lightbender62 <lightbender62@gmail.com>
2026-06-25 07:02:59 +0530

    minor changes

--- black_scholes.py
+++ black_scholes.py
@@ -12,14 +12,13 @@ vol = 0.2 #Volatility (sigma)
 
 def Calculate_d1_d2(S , K , T , r , vol):
     d1 = (math.log(S/K) + (r + 0.5 * vol**2)*T)/(vol * math.sqrt(T))
-
     d2 = d1 - vol*math.sqrt(T)
     return d1 , d2
 
-d1 , d2 = Calculate_d1_d2(S , K , T , r , vol)
 #Calculate Call price & Put Price
 
 def Calculate_Price(S , K , T , r , vol):
+    d1 , d2 = Calculate_d1_d2(S , K , T , r , vol)
     C = S*norm.cdf(d1) - K* math.exp(-r * T ) * norm.cdf(d2)
 
     P = K*math.exp(-r * T)*norm.cdf(-d2) - S*norm.cdf(-d1)
