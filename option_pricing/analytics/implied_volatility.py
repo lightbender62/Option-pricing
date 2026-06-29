dc6a2cc1b820c2a9730a721e4cf3b2b59fce7a81
lightbender62 <lightbender62@gmail.com>
2026-06-29 05:49:00 +0530

    minor fixes

--- option_pricing/analytics/implied_volatility.py
+++ option_pricing/analytics/implied_volatility.py
@@ -13,9 +13,9 @@ def Implied_vol(S , K , T , r , Cm , Pm):
     b2 = False
 
     #Calculation
-    for k in range (max_iter):
+    for _ in range (max_iter):
         # For Call option
-        if(b1 == False):
+        if(not b1):
             C,P = Calculate_Price(S , K , T ,r , vol_old_C)
             v = Vega(S , K , T , r , vol_old_C)
 
@@ -23,12 +23,12 @@ def Implied_vol(S , K , T , r , Cm , Pm):
 
             if(abs(vol_new_C - vol_old_C) < tol):
                 vol_old_C = vol_new_C
-                b1 = True;
+                b1 = True
             else:
                 vol_old_C = vol_new_C
 
         #For Put option
-        if(b2 == False):
+        if(not b2):
             C , P = Calculate_Price(S , K , T ,r , vol_old_P)
             v = Vega(S , K , T , r , vol_old_P)
 
@@ -41,6 +41,6 @@ def Implied_vol(S , K , T , r , Cm , Pm):
                 vol_old_P = vol_new_P
 
         if(b1 and b2):
-            break;
+            break
     
     return vol_old_C , vol_old_P
